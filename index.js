113d7204da993bdfc8be85d660a3836865566ff2
Jonathan Santos <jonathan01347@gmail.com>
2022-08-02 18:14:24 -0300

    feat: foi adicionado a rota /talker e as validações para exibir os dados

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const talker = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +9,18 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const PATH = './talker.json'
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+ const list = await fs.readFile(PATH, 'utf-8')
+ const talkerList = JSON.parse(list)
+  if(talkerList.length > 0) return res.status(200).json(talkerList)
+  return res.status(200).json([])
 });
 
 app.listen(PORT, () => {
